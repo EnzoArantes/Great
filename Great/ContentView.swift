e34dcbf834f3b8438d1a67680f5954ff30985b4e
Zo-hack <enzoarantes2000@gmail.com>
2025-01-14 16:35:48 -0500

    Updates after first lesson

--- Great/ContentView.swift
+++ Great/ContentView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "swift")
+                .resizable()///creates the image to be as large as possible
+                .scaledToFit() ///resizes the image to fit the screen
+           ///code starting with .___ is called a modifier
+                .foregroundStyle(.orange)
+            Text("I am a Programmer")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.red)
         }
         .padding()
     }
